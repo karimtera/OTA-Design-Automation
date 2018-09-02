f1094a153aceb3292e7b33471863126fb876edad
Karim Abdelhady Tera <42269309+karimtera@users.noreply.github.com>
2018-09-02 15:20:25 +0000

    Update addMOS.m
    added some help

--- addMOS.m
+++ addMOS.m
@@ -1,8 +1,14 @@
 function netlist = addMOS(netlist,NUMBER,MOS_TXT_BLOCK,DRAIN,GATE,SOURCE,BULK)
 % returns a new netlist, with a MOSFET added
-% 
-% 
-% 
+% parameters explained: 
+% 1) netlist: is the netlist to be edited
+% 2) NUMBER: used to name the mosfet itself, and its lenght, width
+% 3) MOS_TXT_BLOCK: a char array contains the mosfet template
+% 4) DRAIN: a char array of the node's name connected to drain
+% 5) GATE: a char array of the node's name connected to gate
+% 6) SOURCE: a char array of the node's name connected to source
+% 7) BULK: a char array of the node's name connected to bulk
+
 MOS_TXT_BLOCK = strrep(MOS_TXT_BLOCK,'DRAIN',DRAIN);
 MOS_TXT_BLOCK = strrep(MOS_TXT_BLOCK,'GATE',GATE);
 MOS_TXT_BLOCK = strrep(MOS_TXT_BLOCK,'SOURCE',SOURCE);
