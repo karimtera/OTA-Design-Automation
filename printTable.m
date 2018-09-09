339bdb33d66d42dade6b798361af2dcf4b81f634
Karim Abdelhady Tera <42269309+karimtera@users.noreply.github.com>
2018-09-09 12:39:25 +0000

    Update printTable.m

--- printTable.m
+++ printTable.m
@@ -1,8 +1,14 @@
-function printTable(M,mode)
+function printTable(model)
 % print transistors parameters in table
 
-if mode=='5T_OTA'
+for i=1:11
+   if model.flag(i) == -2
+       return;
+   end
+end
+
+if model.mode=='5T_OTA'
     parameters = {'L','W','GM_ID','GM_GDS','ID','VGS','VDSAT','GM','GDS'};
-    T = struct2table(M,'RowNames',{'M1','M2','M3','M4','M5','M6'});
+    T = struct2table(model.M,'RowNames',{'M1','M2','M3','M4','M5','M6'});
     T = T(:,parameters)
 end
