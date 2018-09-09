f7da93583226d3de3c174c6eb856229e19ece867
Karim Abdelhady Tera <42269309+karimtera@users.noreply.github.com>
2018-09-09 12:41:12 +0000

    Update simResults.m

--- simResults.m
+++ simResults.m
@@ -1,14 +1,19 @@
-function simResults(sim,mode)
-% this function plots the simulation results
+function simResults(model)
+% this function plots the model.simulation results
 
+for i=1:11
+   if model.flag(i) == -2
+       return;
+   end
+end
 
-if mode=='5T_OTA'
-    T = table(sim.AodB,sim.BW,sim.GBW,sim.PM,sim.AoCL,sim.BWCL,sim.GBWCL,'RowNames',{'5T_OTA'},'VariableNames',{'OPENLOOP_GAIN_dB' 'OPENLOOP_BW' 'OPENLOOP_GAINBW' 'PHASE_MARGIN' 'CLOSEDLOOP_GAIN_dB' 'CLOSEDLOOP_BW' 'CLOSEDLOOP_GAINBW'})
+if model.mode=='5T_OTA'
+    T = table(model.sim.AodB,model.sim.BW,model.sim.GBW,model.sim.PM,model.sim.AoCL,model.sim.BWCL,model.sim.GBWCL,'RowNames',{'5T_OTA'},'VariableNames',{'OPENLOOP_GAIN_dB' 'OPENLOOP_BW' 'OPENLOOP_GAINBW' 'PHASE_MARGIN' 'CLOSEDLOOP_GAIN_dB' 'CLOSEDLOOP_BW' 'CLOSEDLOOP_GAINBW'})
     figure 
     subplot(2,1,1)
-    plot(log10(sim.freq),sim.LGmagdB);
+    plot(log10(model.sim.freq),model.sim.LGmagdB);
     title('LOOP GAIN (dB)');
     subplot(2,1,2)
-    plot(log10(sim.freq),angle(sim.LG.LOOPGAIN)*180/pi);
+    plot(log10(model.sim.freq),angle(model.sim.LG.LOOPGAIN)*180/pi);
     title('OPEN LOOP GAIN (PHASE)');
 end
