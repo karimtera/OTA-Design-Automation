fe1b30d25f4cd890b7ca4e85666374710b72dc83
Karim Abdelhady Tera <42269309+karimtera@users.noreply.github.com>
2018-10-14 00:09:46 +0000

    Delete designInputPairGivenGBW.m

--- designInputPairGivenGBW.m
+++
@@ -1,9 +0,0 @@
-function M = designInputPairGivenGBW(type,ID,AO,VDS,CL_EFF,GBW)
-% this function design input pair transistors assuming that both
-% transistors M1 and M2 have the same output conductance
-
-GM = GBW*2*pi*CL_EFF;
-GDS = GM/2/AO;
-M.GM_ID = GM/ID;
-M.L = lookupL(type,M.GM_ID,GDS,ID,VDS,0,1);
-M.ID = ID;
